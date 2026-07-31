0624cae011665c518fd84f874629a59ead4f9d99
Roman Nerez <romannerez@MacBook-Pro-Roman.local>
2026-07-31 19:28:09 +0300

    Project container added

---
+++ app/Containers/AppSection/Project/Models/Project.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Containers\AppSection\Project\Models;
+
+use App\Ship\Parents\Models\Model as ParentModel;
+
+final class Project extends ParentModel
+{
+}
--- app/Containers/AppSection/Project/UI/WEB/Routes/Projects.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Containers\AppSection\Project\UI\WEB\Controllers\ProjectsController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:web')->group(static function (): void {
-    Route::get('projects', [ProjectsController::class, 'showList'])
-        ->name('projects.list');
-});
